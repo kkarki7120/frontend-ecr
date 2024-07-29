27e7c347a900875c57af991b7db249f00b7ec170
kkarki7120 <kkarki7120@gmail.com>
2024-07-29 16:17:55 +0545

    changes in stage

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello this is qa
+          Hello this is stage
         </a>
         <Form/>
       </header>
