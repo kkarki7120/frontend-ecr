b0a4e24ab43b00dc966626d82a2b7a5bef5a8a1d
kkarki7120 <kkarki7120@gmail.com>
2024-07-30 13:52:16 +0545

    changes in dev

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -23,7 +23,7 @@ const FormComponent = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://api-kushal-dev.learn.cloudlaya.com/submit', formData);
+      const response = await axios.post('http://18.143.77.71:4000/submit', formData);
       setResponseMessage(response.data.message);
     } catch (error) {
       setResponseMessage('An error occurred');
