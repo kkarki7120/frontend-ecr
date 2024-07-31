b9badc726ee268a82a086cbc8da3fe5c0ae61ec8
kkarki7120 <kkarki7120@gmail.com>
2024-07-31 11:06:10 +0545

    changes in dev

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello this is stage
+          Hello this is dev
         </a>
         <Form/>
       </header>
